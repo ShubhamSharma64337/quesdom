917efb08056b74ebef24b07aae15e0ca97153054
Shubham Sharma <shubhamsharma64337@gmail.com>
2022-06-22 19:17:28 +0530

    Added some templates and bootstrap

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+    
 if __name__ == "__main__":
     app.run(debug=True)
